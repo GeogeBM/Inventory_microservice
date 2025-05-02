2f353af1caa357333e4fe059e30496ee6f32ca2f
GeorgeBM <jbetma@gmail.com>
2025-05-01 23:36:37 -0600

    quitandor imports

--- inventario/src/main/java/com/xiom/Inventario/API/ConectionApi.java
+++ inventario/src/main/java/com/xiom/Inventario/API/ConectionApi.java
@@ -1,15 +1,8 @@
 package com.xiom.Inventario.API;
-
-import com.xiom.Inventario.models.Producto;
-import com.xiom.Inventario.service.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
